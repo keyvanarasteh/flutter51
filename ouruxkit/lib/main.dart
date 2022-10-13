efdf4ed02e14cce4971cd126829910bf40bdcf25
PC LAB 2B07 <pclab.2b07@istinye.edu.tr>
2022-10-13 20:11:52 +0300

    Scaffold & Widgets

--- ouruxkit/lib/main.dart
+++ ouruxkit/lib/main.dart
@@ -12,10 +12,51 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Ana Sayfa"),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Image.network(
+                'https://i.redd.it/3f461bppquy21.png',
+                fit: BoxFit.contain,
+              ),
+              Icon(Icons.qr_code),
+              Icon(Icons.r_mobiledata),
+              Icon(Icons.e_mobiledata_rounded),
+              Icon(Icons.wallet),
+              Icon(Icons.javascript),
+            ],
+          ),
+        ),
+        drawer: Drawer(
+          child: Text('Menu'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+          currentIndex: 0,
+          selectedItemColor: Colors.amber[800],
+        ),
+      ),
     );
   }
 }
