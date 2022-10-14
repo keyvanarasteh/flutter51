e3f9d60b11d282eecab8b9957789e0decf68b0ce
PC LAB 2B07 <pclab.2b07@istinye.edu.tr>
2022-10-14 14:40:09 +0300

    Work Environmenr Çalişma Alaniniz.

---
+++ ouruxkit/lib/work.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Work extends StatelessWidget {
+  const Work({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(); // BURASI SIZI BEKLIYOR
+  }
+}
+
+// Container(child:...)
+// Center(child:...)
+// Text('Yazi')
+// Image.network('address')
+// Image.assets('address')
+// Icon(Icons.queue_rounded)
+
+// Row(children:[])
+// Column(children:[])
