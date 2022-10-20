a08582bec474c63cd138ea1e22a5fff8f0fea3b3
keyvanarasteh <keyvan.arasteh@live.com>
2022-10-20 20:20:59 +0300

    col row

--- ouruxkit/lib/home_screen.dart
+++ ouruxkit/lib/home_screen.dart
@@ -5,16 +5,37 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.yellow,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
-          Text('1'),
-        ],
+    return SafeArea(
+      child: Container(
+        height: 200,
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CircleAvatar(
+              radius: 80,
+              backgroundImage: NetworkImage(
+                  'https://m.media-amazon.com/images/M/MV5BOTBhMTI1NDQtYmU4Mi00MjYyLTk5MjEtZjllMDkxOWY3ZGRhXkEyXkFqcGdeQXVyNzI1NzMxNzM@._V1_.jpg'),
+            ),
+            Column(
+              children: const [
+                CircleAvatar(
+                  radius: 35,
+                  backgroundImage: NetworkImage(
+                      'https://m.media-amazon.com/images/M/MV5BOTBhMTI1NDQtYmU4Mi00MjYyLTk5MjEtZjllMDkxOWY3ZGRhXkEyXkFqcGdeQXVyNzI1NzMxNzM@._V1_.jpg'),
+                ),
+                CircleAvatar(
+                  radius: 35,
+                  backgroundImage: NetworkImage(
+                      'https://m.media-amazon.com/images/M/MV5BOTBhMTI1NDQtYmU4Mi00MjYyLTk5MjEtZjllMDkxOWY3ZGRhXkEyXkFqcGdeQXVyNzI1NzMxNzM@._V1_.jpg'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
