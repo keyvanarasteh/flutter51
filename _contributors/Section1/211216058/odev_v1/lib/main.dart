ddce8ccdd52425975e53db61ae57a6131c2a968f
Mehmet Can Kaya <92443831+MehmetJank@users.noreply.github.com>
2022-11-17 00:31:41 +0300

    211216058

--- _contributors/Section1/211216058/odev_v1/lib/main.dart
+++ _contributors/Section1/211216058/odev_v1/lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors_in_immutables, library_private_types_in_public_api, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+// to be continued
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
